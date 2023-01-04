fe96a013d3df44beafd34af71d9f3d9d6fcede69
Myko0010 <mikiyastekalegn@gmail.com>
2023-01-04 21:10:33 +0300

    create the landing page of the student

--- app/src/main/java/com/example/androidquizapp/LoginPage.java
+++ app/src/main/java/com/example/androidquizapp/LoginPage.java
@@ -2,6 +2,7 @@ package com.example.androidquizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +26,8 @@ public class LoginPage extends AppCompatActivity {
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                    Toast.makeText(LoginPage.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginPage.this,MainActivity.class);
+                    startActivity(i);
               } else {
                    Toast.makeText(LoginPage.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                }
