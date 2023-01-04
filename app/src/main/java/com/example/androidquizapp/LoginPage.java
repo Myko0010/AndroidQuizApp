2c92c520e61d3763ac10f42012113cf4945db0f6
Myko0010 <mikiyastekalegn@gmail.com>
2023-01-04 19:58:52 +0300

    login modified

--- app/src/main/java/com/example/androidquizapp/LoginPage.java
+++ app/src/main/java/com/example/androidquizapp/LoginPage.java
@@ -3,12 +3,27 @@ package com.example.androidquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
