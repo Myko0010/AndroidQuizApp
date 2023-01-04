5a4feb3c3d525fadd8564d17b28be060f8d0457b
mikiyas <120255855+mik123s@users.noreply.github.com>
2023-01-04 09:59:41 -0800

    Create QuestionsBank.java

---
+++ app/src/main/java/com/example/androidquizapp/QuestionsBank.java
@@ -0,0 +1,101 @@
+package com.example.offlinequiz;
+
+
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionsBank {
+    private static List<QuestionsList> javaQuestions() {
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        final QuestionsList question1 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question2 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question3 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question4 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question5 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question6 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+
+    }
+    private static List<QuestionsList> phpQuestions() {
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        final QuestionsList question1 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question2 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question3 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question4 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question5 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question6 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+    }
+    private static List<QuestionsList> htmlQuestions() {
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        final QuestionsList question1 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question2 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question3 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question4 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question5 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question6 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+    }
+    private static List<QuestionsList> androidQuestions() {
+        final List<QuestionsList> questionsLists = new ArrayList<>();
+
+        final QuestionsList question1 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question2 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question3 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question4 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question5 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        final QuestionsList question6 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
+        questionsLists.add(question1);
+        questionsLists.add(question2);
+        questionsLists.add(question3);
+        questionsLists.add(question4);
+        questionsLists.add(question5);
+        questionsLists.add(question6);
+
+        return questionsLists;
+    }
+
+    /**
+     * @param selectedTopicName
+     * @return
+     */
+    public static List<QuestionsList> getQuestions(String selectedTopicName){
+        switch (selectedTopicName){
+            case "java":
+                return javaQuestions();
+            case "php":
+                return phpQuestions();
+            case "android":
+                return androidQuestions();
+            default:
+                return htmlQuestions();
+        }
+    }
+}
