ac7d163a2b1462a89de352ab2dc5b634dff1578e
Myko0010 <mikiyastekalegn@gmail.com>
2023-01-04 20:05:11 +0300

    test login page

--- app/src/main/java/com/example/androidquizapp/LoginPage.java
+++ app/src/main/java/com/example/androidquizapp/LoginPage.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
     EditText username;
@@ -22,8 +23,13 @@ public class LoginPage extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                   Toast.makeText(LoginPage.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+              } else {
+                   Toast.makeText(LoginPage.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+               }
             }
         });
+
     }
 }
