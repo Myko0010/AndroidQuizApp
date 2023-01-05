c2f880f36e99f65ed9a5008abde49703b1101d97
Getinet Alemu Mijena <alemuamanuel55@gmail.com>
2023-01-05 20:14:38 +0300

    Connecting Registration with database

--- app/src/main/java/com/example/androidquizapp/Registration.java
+++ app/src/main/java/com/example/androidquizapp/Registration.java
@@ -3,12 +3,37 @@ package com.example.androidquizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Registration extends AppCompatActivity {
+
+    EditText etFirstName,etLastName,etUid;
+    RadioGroup radioGroup;
+    RadioButton radioButtonMale;
+    RadioButton radioButtonFemale;
+    Button rgButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        etFirstName = findViewById(R.id.firstName);
+        etLastName = findViewById(R.id.lastName);
+        etUid = findViewById(R.id.uid);
+
+        rgButton = findViewById(R.id.registerButton);
+
+        rgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
