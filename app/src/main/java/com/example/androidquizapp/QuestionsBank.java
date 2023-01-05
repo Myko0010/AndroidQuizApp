bc17af83add2307e67b7b2a0ca02aaecf06c341a
Myko0010 <mikiyastekalegn@gmail.com>
2023-01-05 23:05:10 +0300

    update database

--- app/src/main/java/com/example/androidquizapp/QuestionsBank.java
+++ app/src/main/java/com/example/androidquizapp/QuestionsBank.java
@@ -2,27 +2,42 @@ package com.example.androidquizapp;
 
 
 
+import android.database.Cursor;
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuestionsBank {
+public class QuestionsBank extends LoginPage {
+    static Database database2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_question);
+        database2 = new Database(this);
+    }
+
     private static List<QuestionsList> javaQuestions() {
+
         final List<QuestionsList> questionsLists = new ArrayList<>();
 
-        final QuestionsList question1 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
-        final QuestionsList question2 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
-        final QuestionsList question3 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
-        final QuestionsList question4 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
-        final QuestionsList question5 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
-        final QuestionsList question6 = new QuestionsList("a", "b", "c", "d", "e", "b", "");
-        questionsLists.add(question1);
-        questionsLists.add(question2);
-        questionsLists.add(question3);
-        questionsLists.add(question4);
-        questionsLists.add(question5);
-        questionsLists.add(question6);
+        Cursor cursor=database2.GetSpecificQuestion("Chapter 1");
+        while(cursor.moveToNext()){
+            String question=cursor.getString(2);
+            String choiceA=cursor.getString(3);
+            String choiceB=cursor.getString(4);
+            String choiceC=cursor.getString(5);
+            String choiceD=cursor.getString(6);
+            String Answer=cursor.getString(7);
+            final QuestionsList question1 = new QuestionsList(question, choiceA, choiceB, choiceC, choiceD, Answer, "");
+            questionsLists.add(question1);
+
+        }
+
+
 
         return questionsLists;
 
@@ -97,5 +112,10 @@ public class QuestionsBank {
             default:
                 return htmlQuestions();
         }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
