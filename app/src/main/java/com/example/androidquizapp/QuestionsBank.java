f3a29addc472e0b5100b5a513c240fb886a84a91
Myko0010 <mikiyastekalegn@gmail.com>
2023-01-07 13:09:15 +0300

    Update QuestionsBank.java

--- app/src/main/java/com/example/androidquizapp/QuestionsBank.java
+++ app/src/main/java/com/example/androidquizapp/QuestionsBank.java
@@ -15,32 +15,10 @@ public class QuestionsBank extends LoginPage {
 
        // database2=new Database(this);
     }
-    public Cursor getinet(){
-        Database database2=new Database(this);
-        Cursor cursor =  database2.GetSpecificQuestion("Chapter1");
-        return cursor;
-    }
+
 
     private static List<QuestionsList> javaQuestions() {
-           QuestionsBank tsest=new QuestionsBank();
-           Cursor cursor2=tsest.getinet();
-
-        final List<QuestionsList> questionsLists = new ArrayList<>();
-        while(cursor2.moveToNext()){
-          String question,choiceA,choiceB,choiceC,choiceD,Answer;
-          question=cursor2.getString(2);
-            choiceA=cursor2.getString(3);
-            choiceB=cursor2.getString(4);
-            choiceC=cursor2.getString(5);
-            choiceD=cursor2.getString(6);
-            Answer=cursor2.getString(7);
-            final QuestionsList question1 = new QuestionsList(""+question, ""+choiceA, ""+choiceB, ""+choiceC, ""+choiceD, ""+Answer, "");
-            questionsLists.add(question1);
-
-        }
-
-
-
+        
         final QuestionsList question1 = new QuestionsList("1, Android is -", "A. an operating system", "B.  a web browser", "C. a web browser", "D. None", "A. an operating system", "");
         final QuestionsList question2 = new QuestionsList("2, For which of the following Android is mainly developed?", "A.  for Servers", "B. for Dektops", "C. for laptops", "D. for mobile devices", "D. for mobile devices", "");
         final QuestionsList question3 = new QuestionsList("3,  Which of the following virtual machine is used by the Android operating system?", "A. JVM", "B. DVM", "C. Simple virtual machine", "D. None", "B. DVM", "");
